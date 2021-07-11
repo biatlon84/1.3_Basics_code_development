15799269cae5eb8804f5b98dab41014057c4536e
kwas <kwas@kwas-MS-7641>
2021-07-11 13:28:14 +0300

    1.3_Basics_code_development

--- Task_1/src/com/epam/training/sum/Main.java
+++ Task_1/src/com/epam/training/sum/Main.java
@@ -1,6 +1,40 @@
 package com.epam.training.sum;
 
+import java.util.Scanner;
+
+//The user enters any positive integer.
+//And the program adds up all numbers from one to 
+//the number entered by the user
 public class Main {
-	///
+	public static void main(String[] args) {
+		int a;
+		int z = 0;
+		Scanner scn = new Scanner(System.in);
+		System.out.println("type value of A\n>>");
+		a = TypingInt(scn);
+		// -----
+		for (int i = 1; i <= a; i++) {
+			z += i;
+		}
+
+		System.out.println("The answer is\n>>" + z);
+	}
+
+	public static int TypingInt(Scanner sc) {
+		int a;
+		while (true) {
+			while (!sc.hasNextInt()) {
+				System.out.print(" \"" + sc.next() + "\"");
+				System.out.print(" it is't type INT\n>>");
+			}
+			a = sc.nextInt();
+			if (a <= 1) {
+				System.out.print(" wrong \n>>");
+			} else {
+				break;
+			}
+		}
+		return a;
+	}
 
 }
