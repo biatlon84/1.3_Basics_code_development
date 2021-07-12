25bffd1fd62fe9a79a54ecc99b03f168ad0708fe
kwas <kwas@kwas-MS-7641>
2021-07-12 11:45:35 +0300

    1.3_Basics_code_development

---
+++ Task4/src/com/epam/training/sum/Main.java
@@ -0,0 +1,28 @@
+
+package com.epam.training.sum;
+
+import java.math.BigInteger;
+
+//Multiplications of square first 200 numbers(Task_4)
+public class Main {
+	public static void main(String[] args) {
+		String be = "1";
+		int x;
+		long b = 1L;
+		// ------
+		x = 200;
+		// ------
+		System.out.println("Multiplications of square first " + x + " numbers(without zero)");
+
+		while (b <= x) {
+			long s;
+			s = b * b;
+			BigInteger sq = new BigInteger(String.valueOf(s));
+			BigInteger mul = sq.multiply(new BigInteger(be));
+			be = mul.toString();
+			b++;
+		}
+		System.out.println(be);
+	}
+
+}
